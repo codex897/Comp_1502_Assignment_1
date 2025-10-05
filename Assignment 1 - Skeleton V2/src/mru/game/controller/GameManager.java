6a13b2c4c6f433d4de05fc34e457b560347e2b24
Lorenzo <jstam472@gmail.com>
2025-10-04 21:51:48 -0600

    Still working on the menu Controller Logic

--- Assignment 1 - Skeleton V2/src/mru/game/controller/GameManager.java
+++ Assignment 1 - Skeleton V2/src/mru/game/controller/GameManager.java
@@ -1,5 +1,10 @@
 package mru.game.controller;
 
+import java.util.ArrayList;
+
+import mru.game.model.Player;
+
+import mru.game.view.AppMenu;
 
 public class GameManager {
 	
@@ -11,8 +16,100 @@ public class GameManager {
 	 * A method to find the top players
 	 * Depending on your designing technique you may need and you can add more methods here 
 	 */
+	ArrayList<Player> players;
+	AppMenu menu;
+	
+	//Constructor
+	public GameManager(){
+		System.out.println("\t\tGamemaneger Check");
+		
+		menu = new AppMenu();
+		players = new ArrayList<>();
+		
+		
+		startApp();
+		
+		
+	}
 
-	//TESTINGGNGNGKENFDN
-	//Hellow WORLD
+	//methods start here
+	
+	private void startApp() {
+		while(true) {
+			
+			String userInput= menu.showMainMenu(); //this shows menu  and validates  the input in the menu class
+			
+			switch (userInput) {
+				case "1":
+					playGame();
+					break;
+					
+					
+				case "2":
+					searchByName();
+					
+					break;
+				case "3":
+					save();
+					return; //exit and stops the code
+				
+				default:
+					menu.showInputErrorMessage();
+			}
+		}
+	}
+
+			
+
+		
+		
+	private void searchByName() {
+		String userInputSearch = menu.showSearchMenu(); //this shows search menu  and validates  the input in the menu class
+		switch (userInputSearch) {
+			case "1":
+				searchTop();
+				break;
+			
+			case "2": 
+				searchName();
+				break;
+			
+			case "3":
+				return; //
+				
+			default:
+				menu.showInputErrorMessage();
+		
+	}
+
+
+
+
+
+	}
+
+	private void playGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void searchName() {
+		// TODO logic to search for name
+		menu.showAskName();
+		
+	}
+
+	private void searchTop() {
+		// TODO logic to search for the top
+		menu.showSearchTop();
+		
+	}
+	
+	
 
 }
--- Assignment 1 - Skeleton V2/src/mru/game/view/AppMenu.java
+++ Assignment 1 - Skeleton V2/src/mru/game/view/AppMenu.java
@@ -4,35 +4,57 @@ import java.util.Scanner;
 
 public class AppMenu {
 		
+	/**
+	 * This class will be used to show the menus and sub menus to the user
+	 * It also prompts the user for the inputs and validates them 
+	 */
 	Scanner input;
-		// TEST
 	public AppMenu(){
 		input = new Scanner(System.in);
 	}
 		
 		
-	public void showMainMenu() {
+	public String showMainMenu() {
 		System.out.println("Select one option:");
 		System.out.println("\t1. Play Game");
 		System.out.println("\t2. Search");
 		System.out.println("\t3. Save and Exit\n");
 		System.out.println("Enter a number here: ");
+		
+		return input.nextLine();
 
 	}
 	
-	public void showSubMenu() {
+	public String showSearchMenu() {
 		System.out.println("Select one option:\n");
 		System.out.println("\t1. Top Player");
 		System.out.println("\t2. Search by Name");
 		System.out.println("\t3. Back to Main Menu\n");
 		System.out.println("Enter a number here: ");
 
+		return input.nextLine();
+
+	}
+
+	public String showAskName() {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a name here:");
+		System.out.println("Press enter to go back to Main Menu");
+		return  input.nextLine();
+	}
+
+
+	public String showSearchTop() {
+		System.out.println("top player is");
+		System.out.println("Press enter to go back to Main Menu");
+		return  input.nextLine();
 		
 	}
-	
-	public void promptName() {
-		System.out.println("Enter a name here:");
 
+
+	public void showInputErrorMessage() {
+		System.out.println("Invalid Input, Try again");
+		
 	}
 
 
