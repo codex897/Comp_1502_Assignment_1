9afe9211b26462ab7f38db58916d721bd34c440a
Lorenzo <jstam472@gmail.com>
2025-10-16 18:22:35 -0600

    added the test classes

---
+++ Assignment 1 - Skeleton V2/test/mru/test/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package mru.test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
