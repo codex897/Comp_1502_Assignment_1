6c1ba4a1225a5e8677e0e4fc3032d26632256e2b
codex897 <ayose897@mtroyal.ca>
2025-10-16 23:45:37 -0600

    card test

--- Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/GameManagerTest.java
+++ Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/GameManagerTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
+import mru.game.controller.Card;
 import mru.game.controller.GameManager;
 
 class GameManagerTest {
@@ -20,7 +21,31 @@ class GameManagerTest {
 	 * Uncomment the method to try it
 	 */
 	
+	import org.junit.jupiter.api.Test;
+	import static org.junit.jupiter.api.Assertions.*;
+
+	public class CardTest {
+
+	    @Test
+	    void testCardCreation() {
+	        Card card = new Card("Hearts", "Ace");
+	        assertEquals("Hearts", card.getSuit());
+	        assertEquals("Ace", card.getRank());
+	    }
+
+	    @Test
+	    void testFaceCardToString() {
+	        Card queen = new Card("Spades", "Queen");
+	        assertEquals("Queen of Spades", queen.toString());
+	    }
+
+	    @Test
+	    void testNumberCardToString() {
+	        Card five = new Card("Clubs", "5");
+	        assertEquals("5 of Clubs", five.toString());
+	    }
 	
+
 //	@Test
 //	void testConstructor() throws IOException { //to test if  the gameManager runs press "e" after when testing
 //		GameManager gameManager = new GameManager();
