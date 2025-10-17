563f582900a5e83be2097311a70f47f67d0ee84f
codex897 <ayose897@mtroyal.ca>
2025-10-17 00:20:45 -0600

    new tests

--- Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/CardTest.java
+++ Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/CardTest.java
@@ -1,86 +1,9 @@
 package mru.test.BlackjackGame;
 
-import static org.junit.jupiter.api.Assertions.*;
+	
 
-import org.junit.jupiter.api.Test;
-
-import mru.game.controller.Card;
-
-
-
-/**
- * Tests for the Card class.
- */
-public class CardTest {
-
-    @Test
-    void testCardCreationAndGetters() {
-        Card card = new Card(5, "Hearts");
-        assertEquals(5, card.getRank());
-        assertEquals("Hearts", card.getSuit());
-    }
-
-    @Test
-    void testSettersUpdateValues() {
-        Card card = new Card(7, "Diamonds");
-        card.setRank(9);
-        card.setSuit("Clubs");
-        assertEquals(9, card.getRank());
-        assertEquals("Clubs", card.getSuit());
-    }
-
-    @Test
-    void testToStringForNumberCard() {
-        Card card = new Card(10, "Hearts");
-        assertEquals("10 of Hearts", card.toString());
-    }
-
-    @Test
-    void testToStringForAce() {
-        Card card = new Card(1, "Spades");
-        assertEquals("Ace of Spades", card.toString());
-    }
-
-    @Test
-    void testToStringForJack() {
-        Card card = new Card(11, "Clubs");
-        assertEquals("Jack of Clubs", card.toString());
-    }
-
-    @Test
-    void testToStringForQueen() {
-        Card card = new Card(12, "Diamonds");
-        assertEquals("Queen of Diamonds", card.toString());
-    }
-
-    @Test
-    void testToStringForKing() {
-        Card card = new Card(13, "Hearts");
-        assertEquals("King of Hearts", card.toString());
-    }
-
-    @Test
-    void testGetValueForNumberCards() {
-        Card card = new Card(7, "Hearts");
-        assertEquals(7, card.getValue(), "Number cards should keep their face value");
-    }
-
-    @Test
-    void testGetValueForFaceCards() {
-        Card jack = new Card(11, "Clubs");
-        Card queen = new Card(12, "Diamonds");
-        Card king = new Card(13, "Spades");
-
-        assertEquals(10, jack.getValue(), "Jack should have value 10");
-        assertEquals(10, queen.getValue(), "Queen should have value 10");
-        assertEquals(10, king.getValue(), "King should have value 10");
-    }
-
-    @Test
-    void testGetValueForAce() {
-        Card ace = new Card(1, "Hearts");
-        assertEquals(1, ace.getValue(), "Ace should have base value of 1");
-    }
-}
+	void test() {
+		fail("Not yet implemented");
+	}
 
 
--- Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/DealerTest.java
+++ Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/DealerTest.java
@@ -1,14 +1,58 @@
 package mru.test.BlackjackGame;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import mru.game.controller.Card;
+import mru.game.controller.Dealer;
 
-class DealerTest {
+public class DealerTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private Dealer dealer;
 
+    @BeforeEach
+    void setUp() {
+        dealer = new Dealer();
+    }
+
+    @Test
+    void testDealerStartsWithEmptyHand() {
+        assertTrue(dealer.getHand().isEmpty(), "Dealer hand should start empty");
+    }
+
+    @Test
+    void testAddCardToHand() {
+        Card card = new Card(1, "Hearts");
+        dealer.addCardToHand(card);
+
+        List<Card> hand = dealer.getHand();
+        assertEquals(1, hand.size(), "Dealer hand should have 1 card after adding");
+        assertEquals(card, hand.get(0), "Dealer hand should contain the added card");
+    }
+
+    @Test
+    void testAddMultipleCardsToHand() {
+        dealer.addCardToHand(new Card(10, "Spades"));
+        dealer.addCardToHand(new Card(12, "Clubs"));
+
+        assertEquals(2, dealer.getHand().size(), "Dealer should have 2 cards after adding");
+    }
+
+    @Test
+    void testEmptyHand() {
+        dealer.addCardToHand(new Card(5, "Diamonds"));
+        dealer.addCardToHand(new Card(3, "Hearts"));
+        dealer.emptyHand();
+
+        assertTrue(dealer.getHand().isEmpty(), "Dealer hand should be empty after clearing");
+    }
+
+    @Test
+    void testGetHandReturnsSameListReference() {
+        List<Card> firstRef = dealer.getHand();
+        List<Card> secondRef = dealer.getHand();
+
+        assertSame(firstRef, secondRef, "getHand() should return the same list reference");
+    }
 }
