1de0955d02d4c0d2ddd04389dc0cdd0169cb2741
Lorenzo <jstam472@gmail.com>
2025-10-16 20:45:59 -0600

    more Junit Test

---
+++ Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/GameManagerTest.java
@@ -0,0 +1,30 @@
+package mru.test.BlackjackGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import mru.game.controller.GameManager;
+
+class GameManagerTest {
+
+	//most methods and instance field are set to private 
+	
+	/*
+	 * testConstructor works but the game must launch and press "e" immediately
+	 * This means that it is not automatic like what the pdf instruction said
+	 * so it is not needed
+	 * 
+	 * Uncomment the method to try it
+	 */
+	
+	
+//	@Test
+//	void testConstructor() throws IOException { //to test if  the gameManager runs press "e" after when testing
+//		GameManager gameManager = new GameManager();
+//		assertNotNull(gameManager);
+//	}
+
+}
