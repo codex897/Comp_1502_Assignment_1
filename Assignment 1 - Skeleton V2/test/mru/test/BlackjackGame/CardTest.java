2c87082f01001f9c090196d7032102dcba6410a4
codex897 <ayose897@mtroyal.ca>
2025-10-19 15:10:52 -0600

    test

--- Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/CardTest.java
+++ Assignment 1 - Skeleton V2/test/mru/test/BlackjackGame/CardTest.java
@@ -1,9 +1,14 @@
 package mru.test.BlackjackGame;
 
-	
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+class CardTest {
+
+	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
 
-
+}
